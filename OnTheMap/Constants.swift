7dae36f2413d7db6af18c9d964c7a155da6595d3
Sabrina Svedin <sabrina.svedin@gmail.com>
2019-03-30 12:19:03 -0700

    Add Parse API params

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  OnTheMap
+//
+//  Created by Sabrina on 3/30/19.
+//  Copyright © 2019 Sabrina Svedin. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Parse {
+        static let ApplicationId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let APIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    }
+}
